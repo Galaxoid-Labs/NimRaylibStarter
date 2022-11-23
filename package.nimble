eb902a60aa03a85346c961ef85515f423bfbb046
Jacob Davis <ismyhc@gmail.com>
2022-11-23 09:49:20 -0500

    inital commit

---
+++ package.nimble
@@ -0,0 +1,14 @@
+# Package
+
+version       = "0.1.0"
+author        = "Jacob Davis"
+description   = "Simple Starter Template for Raylib"
+license       = "MIT"
+srcDir        = "src"
+bin           = @["main=game"]
+
+
+# Dependencies
+
+requires "nim >= 1.6.8"
+requires "nimraylib_now"
---
+++ src/main.nim
@@ -0,0 +1,44 @@
+import nimraylib_now
+import game
+
+# IMPORTANT
+# You should'nt have to change anything here for the most part
+# Write you game logic in game.nim
+
+when isMainModule:
+
+  setConfigFlags(flags)
+  setTargetFPS(targetFPS)
+  initWindow(windowWidth, windowHeight, title)
+  setWindowMinSize(minWindowWidth, minWindowHeight)
+
+  let target = loadRenderTexture(renderWidth, renderHeight)
+  setTextureFilter(target.texture, textureFitler)
+
+  start()
+
+  while not windowShouldClose():
+
+    update(getFrameTime())
+
+    beginTextureMode(target)
+    draw()
+    endTextureMode()
+
+    beginDrawing()
+    clearBackground(letterBoxColor)
+
+    let scale: float = min(getScreenWidth() / renderWidth, getScreenHeight() / renderHeight)
+    let sourceRect: Rectangle = (0.0, 0.0, target.texture.width.toFloat, -target.texture.height.toFloat)
+    let destY: float = (getScreenWidth().toFloat - (renderWidth.toFloat * scale)) * 0.5
+    let destX: float = (getScreenHeight().toFloat - (renderHeight.toFloat * scale)) * 0.5
+    let destRect: Rectangle = (destY, destX, renderWidth.toFloat * scale, renderHeight.toFloat * scale)
+    drawTexturePro(target.texture, sourceRect, destRect, (0.0, 0.0), 0.0, Raywhite)
+
+    if debug:
+      drawFPS(20, 20)
+    
+    drawGUI()
+    endDrawing()
+
+  closeWindow()
