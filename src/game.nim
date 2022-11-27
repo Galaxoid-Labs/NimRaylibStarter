fc831914cf25c3e1b39545adb2c5faeaf789842d
Jacob Davis <ismyhc@gmail.com>
2022-11-27 16:50:43 -0500

    cleanup

--- src/game.nim
+++ src/game.nim
@@ -2,13 +2,8 @@ import nimraylib_now
 import configs
 import extras
 
-var c: Color
-var p: Vector2
-
 # Do game initialization here
-proc start*(): void = 
-  c = randomPicoColor()
-  p = randomPositionInRenderBounds()
+proc start*(): void = discard
 
 # Do game object updates here
 proc update*(dt: cfloat): void = discard
@@ -16,7 +11,7 @@ proc update*(dt: cfloat): void = discard
 # Do drawing here
 proc draw*(): void =
   clearBackground(ClearScreenColor)
-  
+
 # Important to understand this drawing
 # doesnt scale with the render target size. It draws at the normal windowWidth/windowHeight
 proc drawUnscaled*(): void = discard
